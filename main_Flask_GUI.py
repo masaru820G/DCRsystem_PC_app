2c052ef34be735babae42a7590de10cde655b127
masaru820G <masalu20gm@gmail.com>
2025-12-02 02:54:09 +0900

    1202

--- main_Flask_GUI.py
+++ main_Flask_GUI.py
@@ -28,9 +28,9 @@ from PySide6.QtGui import (
     QPixmap
 )
 
-import patlite_control as pl_ctr              # パトライト制御のモジュール
-import relay_control as RelayContoroller      # リレーボード制御のclassモジュール
-
+import modele_patlite as p_ctr              # パトライト制御のモジュール
+import modele_relay as r_ctr                # リレーボード制御のclassモジュール
+#import modele_cap_video as v_ctr           # カメラキャプチャのモジュール
 
 # --- ラズパイIPアドレス ---
 RPI_IP_ADDRESS = "192.168.2.2"
